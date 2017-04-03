329e2f3501a12f97d5ae288bbd33b4f8d3c8f175
Rob Hoes <rob.hoes@citrix.com>
2017-04-03 17:24:03 +0100

    CP-20603: Pass vGPU fd to xenguest on source host during migration
    When starting a migration of a VM with a vGPU, we need to tell xenguest about
    the file descriptor for the connection that was opened for transferring the
    vGPU state to the destination host. Xenguest will pass this on to DEMU, which
    kicks off the vGPU migration phase.
    
    Do this by adding a new optional parameter to the `VM_save` operation for the
    additional file descriptor.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- xc/xenops_server_xen.ml
+++ xc/xenops_server_xen.ml
@@ -1466,7 +1466,7 @@ module VM = struct
 			)
 		| FD fd -> f fd
 
-	let save task progress_callback vm flags data =
+	let save task progress_callback vm flags data vgpu_data =
 		let flags' =
 			List.map
 				(function
@@ -1482,7 +1482,8 @@ module VM = struct
 				with_data ~xc ~xs task data true
 					(fun fd ->
 						let vm_str = Vm.sexp_of_t vm |> Sexplib.Sexp.to_string in
-						Domain.suspend task ~xc ~xs ~hvm ~progress_callback ~qemu_domid (choose_xenguest vm.Vm.platformdata) vm_str domid fd flags'
+						let vgpu_fd = match vgpu_data with Some (FD fd) -> Some fd | _ -> None in
+						Domain.suspend task ~xc ~xs ~hvm ~progress_callback ~qemu_domid (choose_xenguest vm.Vm.platformdata) vm_str domid fd vgpu_fd flags'
 							(fun () ->
 								if not(request_shutdown task vm Suspend 30.)
 								then raise (Failed_to_acknowledge_shutdown_request);
