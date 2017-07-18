47ec5a4cf39dbdbe7275f023fff6e49bb0c73d32
Edwin Török <edvin.torok@citrix.com>
2017-07-18 16:26:47 +0100

    refactor work queues: add unit test for work queues
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

---
+++ xapi-work-queues/test/main.ml
@@ -0,0 +1,170 @@
+open OUnit2
+open Stdext.Threadext
+
+module Item = struct
+  type handle = {
+    id: string;
+    tag: string;
+    f: unit -> unit;
+    finally: unit -> unit;
+  }
+  type t = string * handle
+
+  module Dump = struct
+    type t = {
+      id: string;
+      tag: string;
+    } [@@deriving rpc]
+  end
+
+  let describe_item (op, _) = op
+
+  let dump_task (_, (task:handle)) =
+    Dump.rpc_of_t {Dump.id = task.id; tag = task.tag}
+
+  let printer s = s
+
+  let execute (op, t) =
+    assert_equal ~printer t.id op;
+    t.f ()
+
+  let finally (op, t) =
+    assert_equal ~printer t.id op;
+    t.finally ()
+
+  let should_keep t _ = true
+end
+
+module Lib_worker = Xapi_work_queues.Make(Item)
+
+type job = {
+  id: string;
+  mutable called:  bool;
+  mutable finalized: bool;
+}
+
+type running = {
+  m: Mutex.t;
+  cond: Condition.t;
+  count: int ref;
+}
+
+let test_pool ~workers ~vms ~events ?(errors=0) ctx =
+  let open Lib_worker in
+  logf ctx `Info "Setting worker pool size to %d" workers;
+  WorkerPool.set_size 0;
+  WorkerPool.start workers;
+  let open Item in
+
+  let running = {
+    m = Mutex.create ();
+    cond = Condition.create ();
+    count = ref 0;
+  } in
+
+  let mutexes = Array.init 2 (fun _ -> Mutex.create ()) in
+
+  let create_handle i =
+    let id =  "handle#" ^ (string_of_int i) in
+    let tag = "vm#" ^ (string_of_int (i mod vms)) in
+    let job = {
+      id; called = false; finalized = false;
+    } in
+    let f () =
+      job.called <- true;
+      Mutex.execute mutexes.(i mod 2) (fun () ->
+          if (i < errors) then
+            failwith "Testing failures"
+        )
+    in
+    let finally () =
+      Mutex.execute running.m (fun () ->
+          decr running.count;
+          Condition.signal running.cond);
+      job.finalized <- true in
+    job, { id; tag; f; finally }
+  in
+
+
+  let jobs_and_handles = Array.init events create_handle in
+  let handles = Array.map snd jobs_and_handles in
+
+  logf ctx `Info "Pushing items onto work queue";
+  (* queue but do not execute jobs, have them locked on the mutex *)
+  Mutex.execute mutexes.(0) (fun () ->
+      Mutex.execute mutexes.(1) (fun () ->
+          Array.iteri (fun i h ->
+              Mutex.execute running.m (fun () -> incr running.count);
+              Redirector.push Redirector.default h.tag (h.id, h)) handles
+        );
+      WorkerPool.set_size 1;
+      WorkerPool.set_size workers;
+    );
+  WorkerPool.Dump.(make () |> rpc_of_t |> Jsonrpc.to_string |> print_endline);
+  Lib_worker.Redirector.Dump.(make () |> rpc_of_t |> Jsonrpc.to_string |> print_endline);
+
+  logf ctx `Info "Waiting for all jobs to be processed";
+  Mutex.execute running.m (fun () ->
+      while !(running.count) > 0 do
+        Condition.wait running.cond running.m
+      done
+    );
+
+  WorkerPool.Dump.(make () |> rpc_of_t |> Jsonrpc.to_string |> print_endline);
+  Lib_worker.Redirector.Dump.(make () |> rpc_of_t |> Jsonrpc.to_string |> print_endline);
+
+  (* queue shutdowns *)
+  WorkerPool.set_size 0;
+  WorkerPool.Dump.(make () |> rpc_of_t |> Jsonrpc.to_string |> print_endline);
+
+  (* workers need one last job to shut down *)
+  for i = 1 to workers do
+    let handle = {
+      id = "shutdown#" ^ (string_of_int i);
+      tag = "";
+      f = (fun () -> ());
+      finally = (fun () ->
+          Mutex.execute running.m (fun () ->
+              decr running.count;
+              Condition.signal running.cond);
+          ());
+    } in
+    Mutex.execute running.m (fun () -> incr running.count);
+    Redirector.push Redirector.default "shutdown" (handle.id, handle);
+    Redirector.push Redirector.parallel_queues "shutdown" (handle.id, handle)
+  done;
+  logf ctx `Info "Waiting for all jobs to be processed";
+  Mutex.execute running.m (fun () ->
+      while !(running.count) > 0 do
+        Condition.wait running.cond running.m
+      done
+    );
+  WorkerPool.Dump.(make () |> rpc_of_t |> Jsonrpc.to_string |> print_endline);
+  WorkerPool.set_size 1;
+  WorkerPool.set_size (-1);
+  WorkerPool.Dump.(make () |> rpc_of_t |> Jsonrpc.to_string |> print_endline);
+  logf ctx `Info "Checking handles";
+
+  let count_expect msg p =
+    let actual = Array.to_list jobs_and_handles |> List.filter p |> List.length in
+    let expected = Array.length jobs_and_handles in
+    assert_equal ~printer:string_of_int actual expected
+  in
+
+  count_expect "should be called" (fun (job, _) -> job.called);
+  count_expect "should be finalized" (fun (job, _) -> job.finalized)
+
+let suite =
+  "xapi-work-queues" >::: [
+    "simple 1" >:: test_pool ~workers:2 ~events:10 ~vms:1;
+    "simple 2" >:: test_pool ~workers:2 ~events:10 ~vms:2;
+    "basic 1 thread" >:: test_pool ~workers:1 ~events:100 ~vms:100;
+    "basic 25 threads" >:: test_pool ~workers:25 ~events:100 ~vms:100;
+    "test per tag queueing" >:: test_pool ~workers:1 ~vms:4 ~events:1000;
+    "test per tag queueing" >:: test_pool ~workers:25 ~vms:4 ~events:1000;
+    "test per tag queueing with errors" >:: test_pool ~workers:25 ~vms:4 ~events:1000 ~errors:14;
+  ]
+
+let () =
+  (*  Debug.log_to_stdout ();*)
+  OUnit2.run_test_tt_main suite
