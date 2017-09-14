xapi-project/xenopsd

commit 42bd3e76a1f5da36a3e4c8ef0d8abee1dfef14dc

CA-264906: Call new domctl for qemu-trad vCPU hotplug
Call the new domctl for vCPU hotplug for both qemu-trad and upstream
QEMU. This is needed to fix vCPU hotplug with qemu-trad since now that
the emulation is done in Xen, the hotplug implementation in qemu-trad
does not take effect.

Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

xc/xenops_server_xen.ml
@@ -1073,8 +1073,9 @@ module VM = struct
 		Domain.set_xsdata ~xs di.Xenctrl.domid xsdata
 	) Newest task vm
 
-	let set_vcpus' task vm target = on_domain (fun xc xs _ _ di ->
+	let set_vcpus task vm target = on_domain (fun xc xs _ _ di ->
 		let domid = di.Xenctrl.domid in
+		Xenctrlext.domain_set_vcpu_hotplug xc domid target;
 		(* Returns the instantaneous CPU number from xenstore *)
 		let current =
 			let n = ref (-1) in
@@ -1096,14 +1097,6 @@ module VM = struct
 				Device.Vcpu.set ~xs ~devid:i domid true
 			done
 		)
-	) Newest task vm
-
-	let set_vcpus task vm ncpus = on_domain (fun xc xs _ _ di ->
-		if Device_common.is_upstream_qemu di.Xenctrl.domid then begin
-			Xenctrlext.domain_set_vcpu_hotplug xc di.Xenctrl.domid ncpus;
-			set_vcpus' task vm ncpus
-		end else
-			set_vcpus' task vm ncpus
 	) Newest task vm
 
 	let set_shadow_multiplier task vm target = on_domain (fun xc xs _ _ di ->

commit f2798050a15157336171916f50734e327b3aa2c8

CA-265618: Fix vCPU hotplug for PV domains
Commit 42bd3e76a1f5 ("CA-264906: Call new domctl for qemu-trad vCPU
hotplug") broke vCPU hotplug for PV domains because it always called the
new domctl but this domctl does not work with PV domains. Fix this by
only calling the new domctl for HVM domains.

Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

xc/xenops_server_xen.ml
@@ -1075,7 +1075,7 @@ module VM = struct
 
 	let set_vcpus task vm target = on_domain (fun xc xs _ _ di ->
 		let domid = di.Xenctrl.domid in
-		Xenctrlext.domain_set_vcpu_hotplug xc domid target;
+		if di.Xenctrl.hvm_guest then Xenctrlext.domain_set_vcpu_hotplug xc domid target;
 		(* Returns the instantaneous CPU number from xenstore *)
 		let current =
 			let n = ref (-1) in