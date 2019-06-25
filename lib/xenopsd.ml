2eecab3f401a7dedfc30e5b679408094204cab4d
Ming Lu <ming.lu@citrix.com>
2019-06-25 14:22:17 +0800

    CP-31431: Add quarantine/dequarantine for PCI devices
    This commit makes changes for CA-297891, in which a special domain is
    used in Xen to quarantine the PCI devices for pass-through. The
    quarantine means a separate I/O address space for DMA of PCI device. Two
    cases are considered in this commit:
    
    1. when a PCI device is going to be passed-through to a guest domain, it
    will be placed into quarantine firstly (done by toolstack). Later on,
    when it is deassigned from the guest domain, it will be placed (done by
    Xen) to quarantine domain, rather than the dom0.
    
    2. when a PCI device, which had been passed-through to a guest domain and
    now it is in quarantine, is going to be managed by host driver running on
    dom0 to emulate virtual devices (not PCI pass-through), it should be
    moved out from quarantine and assigned back to dom0. I.E. NVIDIA vGPUs
    and Intel vGPUs.
    
    Signed-off-by: Ming Lu <ming.lu@citrix.com>
    Signed-off-by: Igor Druzhinin <igor.druzhinin@citrix.com>

--- lib/xenopsd.ml
+++ lib/xenopsd.ml
@@ -38,6 +38,7 @@ let default_vbd_backend_kind = ref "vbd"
 let ca_140252_workaround = ref false
 
 let additional_ballooning_timeout = ref 120.
+let pci_quarantine = ref true
 
 let options = [
     "queue", Arg.Set_string Xenops_interface.queue_name, (fun () -> !Xenops_interface.queue_name), "Listen on a specific queue";
@@ -55,6 +56,7 @@ let options = [
     "default-vbd-backend-kind", Arg.Set_string default_vbd_backend_kind, (fun () -> !default_vbd_backend_kind), "Default backend for VBDs";
     "ca-140252-workaround", Arg.Bool (fun x -> ca_140252_workaround := x), (fun () -> string_of_bool !ca_140252_workaround), "Workaround for evtchn misalignment for legacy PV tools";
     "additional-ballooning-timeout", Arg.Set_float additional_ballooning_timeout, (fun () -> string_of_float !additional_ballooning_timeout), "Time we allow the guests to do additional memory ballooning before live migration";
+   "pci-quarantine", Arg.Bool (fun b -> pci_quarantine := b), (fun () -> string_of_bool !pci_quarantine), "True if IOMMU contexts of PCI devices are needed to be placed in quarantine";
 ]
 
 let path () = Filename.concat !sockets_path "xenopsd"
