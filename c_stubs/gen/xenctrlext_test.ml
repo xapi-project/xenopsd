3e16bc931eaac5ffc0bd971a0a7baa731b5c300a
Edwin Török <edvin.torok@citrix.com>
2019-09-12 18:05:59 +0100

    wip

--- c_stubs/gen/xenctrlext_test.ml
+++ c_stubs/gen/xenctrlext_test.ml
@@ -1,8 +1,8 @@
-open Xenctrlext
+open Xenctrlext2
 
-let meminfo, distances = with_xc Xenctrlext.numainfo
+let meminfo, distances = with_xc Xenctrlext2.numainfo
 
-let cputopo = with_xc Xenctrlext.cputopoinfo
+let cputopo = with_xc Xenctrlext2.cputopoinfo
 
 let () =
   Printf.printf "Nodes: %d\n" (Array.length meminfo);
@@ -12,7 +12,7 @@ let () =
     mi.Meminfo.memsize) meminfo;
   Array.iteri (fun i ->
     Array.iteri (fun j e ->
-      Printf.printf "%d <-> %d: %d\n" i j (Unsigned.UInt32.to_int e)
+      Printf.printf "%d <-> %d: %d\n" i j e
   )) distances;
   Array.iteri (fun i e ->
     Printf.printf "CPU %d -> %d, %d, %d\n"
