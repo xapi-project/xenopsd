258a2d220727a66b05f0a074a35620e960e07e88
Rob Hoes <rob.hoes@citrix.com>
2020-11-12 14:41:04 +0000

    CA-332779: verify power state for start/reboot/resume
    Backport of 5ce4ea2.
    
    Do a VM power-state check before executing VM_reboot, VM_start or VM_resume. Do
    this in `perform`, right before executing the operation, which should be
    exactly the right time. These checks replace similar power-state checks in
    xapi, which are done before queueing the operation. This is too early, because
    xenopsd may be busy responding to in-guest requests to shutdown or reboot.
    
    The checks are done outside the exception handler that surrounds `perform`,
    because we want to just raise an error to the client that requested the
    operation (i.e. xapi) without entering `trigger_cleanup_after_failure` (the VM
    is good, but the request is bad).
    
    On Havana, it is unfortunately necessary to allow a Halted power-state for the
    resume operation rather than just Suspended. This is because it is impossible
    to determine whether such VMs are suspended or not, as the
    `suspend_memory_bytes` field was not yet in the persistent VM state.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- lib/xenops_server.ml
+++ lib/xenops_server.ml
@@ -1439,6 +1439,24 @@ and trigger_cleanup_after_failure_atom op t = match op with
 		| Parallel (id, description, ops) ->
 			List.iter (fun op->trigger_cleanup_after_failure_atom op t) ops
 
+and verify_power_state op =
+	let module B = (val get_backend () : S) in
+	let assert_power_state_is vm_id expected =
+		let power = (B.VM.get_state (VM_DB.read_exn vm_id)).Vm.power_state in
+		if not (List.mem power expected) then
+			let expected' = match expected with x :: _ -> x | [] -> failwith "Expectation missing" in
+			raise (Bad_power_state (power, expected'))
+	in
+	match op with
+	| VM_start (id, _) -> assert_power_state_is id [Halted]
+	| VM_reboot (id, _) -> assert_power_state_is id [Running; Paused]
+	| VM_resume (id, _) ->
+		(* We also accept Halted here: when resuming a pre-Lima VM, the
+			suspend_memory_bytes field in the internal state is always 0, causing
+			B.VM.get_state to return Halted. *)
+			assert_power_state_is id [Suspended; Halted]
+	| _ -> ()
+
 and perform ?subtask ?result (op: operation) (t: Xenops_task.t) : unit =
 	let module B = (val get_backend () : S) in
 	let one = function
@@ -1700,6 +1718,7 @@ and perform ?subtask ?result (op: operation) (t: Xenops_task.t) : unit =
 			perform_atomic ~progress_callback ?subtask ?result op t
 	in
 	let one op =
+		verify_power_state op;
 		try
 			one op
 		with e ->
