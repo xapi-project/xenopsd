2bc279a0647c226b14c5b1a7041d20aff6465ade
Ben Anson <ben.anson@citrix.com>
2020-11-19 09:52:52 +0000

    CA-344431: ls_lR: factor out dir concatenation
    Backport of 58261862244
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>
    Signed-off-by: Ben Anson <ben.anson@citrix.com>

--- xc/xenops_server_xen.ml
+++ xc/xenops_server_xen.ml
@@ -1616,6 +1616,7 @@ module VM = struct
 			) Newest
 
 	let get_state vm =
+    let map_tr f l = List.rev_map f l |> List.rev in
 		let uuid = uuid_of_vm vm in
 		let vme = vm.Vm.id |> DB.read in (* may not exist *)
 		with_xc_and_xs
@@ -1656,9 +1657,11 @@ module VM = struct
 
 						let rtc = try xs.Xs.read (Printf.sprintf "/vm/%s/rtc/timeoffset" (Uuidm.to_string uuid)) with Xs_protocol.Enoent _ -> "" in
 						let rec ls_lR root dir =
-							let this = try [ dir, xs.Xs.read (root ^ "/" ^ dir) ] with _ -> [] in
-							let subdirs = try xs.Xs.directory (root ^ "/" ^ dir) |> List.filter (fun x -> x <> "") |> List.map (fun x -> dir ^ "/" ^ x) with _ -> [] in
-							this @ (List.concat (List.map (ls_lR root) subdirs)) in
+              let entry = root ^ "/" ^ dir in
+              let this = try [(dir, xs.Xs.read entry)] with _ -> [] in
+              let subdirs = try xs.Xs.directory entry |> List.filter (fun x -> x <> "") |> map_tr (fun x -> dir ^ "/" ^ x) with _ -> [] in
+              this @ (List.concat (map_tr (ls_lR root) subdirs)) in
+            in
 						let guest_agent =
 							[ "drivers"; "attr"; "data"; "control"; "feature" ] |> List.map (ls_lR (Printf.sprintf "/local/domain/%d" di.Xenctrl.domid)) |> List.concat |> List.map (fun (k,v) -> (k,Xenops_utils.utf8_recode v)) in
 						let xsdata_state =
