1b99006f21bdcbdfda0e3e0eaca3b81a11c973cf
Edwin Török <edvin.torok@citrix.com>
2021-05-12 10:53:32 +0000

    CA-353746: enable relaxed MSR when migrating from old version

--- xc/domain.ml
+++ xc/domain.ml
@@ -54,7 +54,9 @@ type x86_arch_misc_flags = Xenctrl.x86_arch_misc_flags =
 
 type xen_x86_arch_domainconfig = Xenctrl.xen_x86_arch_domainconfig = {
     emulation_flags: x86_arch_emulation_flags list
-  ; misc_flags: x86_arch_misc_flags list
+  ; misc_flags: x86_arch_misc_flags list [@default [X86_MSR_RELAXED]]
+        (* misc_flags is missing when migrating from old version.
+           set the default to relaxed MSR for backwards compatibility *)
 }
 [@@deriving rpcty]
 
